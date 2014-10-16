840a185563a813c4e3906cd6bb4097bf7871a3f1
Sveinn <sveinnthorhalls@gmail.com>
2014-10-16 14:07:41 +0000

    added functions to parser

--- Parser.java
+++ Parser.java
@@ -2,4 +2,184 @@
  * Created by siddi on 16/10/14.
  */
 public class Parser {
+
+    private void program()
+    {
+
+    }
+
+    private void variable_declarations()
+    {
+
+    }
+
+    private void type()
+    {
+
+    }
+
+    private void variable_list()
+    {
+
+    }
+
+    private void temp_var_list()
+    {
+
+    }
+
+    private void variable()
+    {
+
+    }
+
+    private void variable_left_factor()
+    {
+
+    }
+
+    private void method_declarations()
+    {
+
+    }
+
+    private void more_method_declarations()
+    {
+
+    }
+
+    private void method_declaration()
+    {
+
+    }
+
+    private void method_return_type()
+    {
+
+    }
+
+    private void parameters()
+    {
+
+    }
+
+    private void parameter_list()
+    {
+
+    }
+
+    private void temp_parameter_list()
+    {
+
+    }
+
+    private void statement_list()
+    {
+
+    }
+
+    private void id_starting_statement()
+    {
+
+    }
+
+    private void id_starting_statement_rest()
+    {
+
+    }
+
+    private void statement()
+    {
+
+    }
+
+    private void optional_expression()
+    {
+
+    }
+
+    private void statement_block()
+    {
+
+    }
+
+    private void incr_decr_var()
+    {
+
+    }
+
+    private void optional_else()
+    {
+
+    }
+
+    private void expression_list()
+    {
+
+    }
+
+    private void more_expressions()
+    {
+
+    }
+
+    private void expression()
+    {
+
+    }
+
+    private void ex_left_factor()
+    {
+
+    }
+
+    private void simple_expression()
+    {
+
+    }
+
+    private void temp_simple_expression()
+    {
+
+    }
+
+    private void term()
+    {
+
+    }
+
+    private void temp_term()
+    {
+
+    }
+
+    private void id_starting_factor()
+    {
+
+    }
+
+    private void id_starting_factor_rest()
+    {
+
+    }
+
+    private void factor()
+    {
+
+    }
+
+    private void variable_loc()
+    {
+
+    }
+
+    private void variable_loc_left_factor()
+    {
+
+    }
+
+   private void sign()
+   {
+       
+   }
 }
